3f81ecce7f1a89177ccb224d06b6eb8d3ec62b46
macedobbquente <heloisy.clara@escola.pr.gov.br>
2026-07-09 09:45:51 -0300

    helo linda

---
+++ script.js
@@ -0,0 +1,6 @@
+const curtidas = document.gwtElementById ( 'curtidas') // pegar o 
+// botão de curtidas pelo id
+curtidas .addEventListener ( 'clicl', aumentarCurtidas) //quando
+ clicar //chama a função 
+ //  função de aumentar curtidas
+ 
